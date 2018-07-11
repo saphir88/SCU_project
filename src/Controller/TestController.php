af9343710f30c88da71f46beb89681328b8e3447
Thomas Catella <spectralys88@gmail.com>
2018-07-11 13:52:44 +0200

    début d'integration de coolAdmin

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -2,12 +2,7 @@
 
 namespace App\Controller;
 
-use App\Entity\Article;
-use App\Form\ArticleType;
-use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
