f9e0aac89b548e8ecfe44a70eea16b4006db3e32
Thomas Catella <spectralys88@gmail.com>
2018-07-20 17:50:19 +0200

    page Rank terminée

--- src/Controller/RankController.php
+++ src/Controller/RankController.php
@@ -3,7 +3,6 @@
 namespace App\Controller;
 
 use App\Entity\Rank;
-use App\Form\RankType;
 use App\Repository\RankRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -16,66 +15,36 @@ use Symfony\Component\Routing\Annotation\Route;
 class RankController extends Controller
 {
     /**
-     * @Route("/", name="rank_index", methods="GET")
+     * @Route("/", name="rank_index", methods={"GET","POST"})
+     * @param RankRepository $rankRepository
+     * @return Response
      */
     public function index(RankRepository $rankRepository): Response
     {
+
+        // Modification d'un rang
+        if(isset($_POST['rankName']) and isset($_POST['rankId']) ) {
+            $rankRepository->find($_POST['rankId'])->setName($_POST['rankName']);
+            $this->getDoctrine()->getManager()->flush();
+        }
+
+        // Ajout d'un rang
+        if(isset($_POST['newRankName'])) {
+            $rank = new Rank();
+            $rank->setName($_POST['newRankName']);
+            $this->getDoctrine()->getManager()->persist($rank);
+            $this->getDoctrine()->getManager()->flush();
+            return $this->redirectToRoute('rank_index');
+        }
+
         return $this->render('rank/index.html.twig', ['ranks' => $rankRepository->findAll()]);
     }
 
     /**
-     * @Route("/new", name="rank_new", methods="GET|POST")
-     */
-    public function new(Request $request): Response
-    {
-        $rank = new Rank();
-        $form = $this->createForm(RankType::class, $rank);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($rank);
-            $em->flush();
-
-            return $this->redirectToRoute('rank_index');
-        }
-
-        return $this->render('rank/new.html.twig', [
-            'rank' => $rank,
-            'form' => $form->createView(),
-        ]);
-    }
-
-    /**
-     * @Route("/{id}", name="rank_show", methods="GET")
-     */
-    public function show(Rank $rank): Response
-    {
-        return $this->render('rank/show.html.twig', ['rank' => $rank]);
-    }
-
-    /**
-     * @Route("/{id}/edit", name="rank_edit", methods="GET|POST")
-     */
-    public function edit(Request $request, Rank $rank): Response
-    {
-        $form = $this->createForm(RankType::class, $rank);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $this->getDoctrine()->getManager()->flush();
-
-            return $this->redirectToRoute('rank_edit', ['id' => $rank->getId()]);
-        }
-
-        return $this->render('rank/edit.html.twig', [
-            'rank' => $rank,
-            'form' => $form->createView(),
-        ]);
-    }
-
-    /**
      * @Route("/{id}", name="rank_delete", methods="DELETE")
+     * @param Request $request
+     * @param Rank $rank
+     * @return Response
      */
     public function delete(Request $request, Rank $rank): Response
     {
